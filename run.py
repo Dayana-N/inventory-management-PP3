3e042404c8a128a54130f789869f2f0dba6b4ca2
Dayana <dayana.nashkova@yahoo.com>
2023-04-24 10:25:26 +0000

    feat: add instructions option in the main menu. Displays instructions on how to use the application

--- run.py
+++ run.py
@@ -554,6 +554,62 @@ def welcome_screen():
     ''', justify='center', style='light_steel_blue1')
 
 
+def display_info():
+    '''
+    Displays instructions on how to use
+    the application
+    '''
+    console.print(
+        '''
+        Inventory Management allows you to manage serialized inventory.
+The program can help any business track its serialized stock.
+When you run the program you will be presented with the main menu.\n
+PRESS C TO ADD STOCK
+PRESS V TO VIEW STOCK,
+PRESS S TO SEARCH AND EDIT
+PRESS I FOR INSTRUCTIONS
+PRESS Q TO QUIT \n
+To add stock to the system press C. The system will then ask for
+name of the particular item/stock and serial number. After that, you can
+add the location of the stock:\n
+PRESS W FOR WAREHOUSE
+PRESS J FOR JOB/SITE
+PRESS E TO ASSIGN TO ENGINEER\n
+If you select W for warehouse the system will ask if the stock
+should be added to G for good stock location (working stock) or
+B for bad stock location (faulty stock, broken returns, etc)\n
+If you select J for job/site, the system will ask for the name of
+the site that the stock was installed/shipped to\n
+If you select E for an engineer, the system will ask for the
+name of the engineer that holds this stock.\n
+The system will then check if the serial number exists on the system and if
+it doesn't it will add it to the system and will display all the information
+added. If the serial number already exists the system will display a message
+in red letting the user know that the serial number already exists and will
+redirect to the main menu.\n
+If you wish to view stock from the main menu press V. The system will ask if
+you wish to view all stock, or by location and will then display the data in
+a table.\n
+If you wish to search and edit you can do so by pressing S to search.
+The system will ask for the serial number and will search the system. If the
+serial number is not found the system will let the user know and will redirect
+to the main menu. If the serial number is found it will be displayed in
+a table. You will can then \n
+PRESS:
+S TO SEARCH AGAIN
+E TO EDIT STOCK LOCATION
+D TO DELETE
+M FOR MAIN MENU\n
+To edit the location of the displayed serial number press E. The system will
+ask for the new location and will update the system.\n
+To delete the displayed serial number press D. The system will ask if you wish
+to proceed and will delete the serial number permanently from the system.\n
+Full documentation can be found at:
+https://github.com/Dayana-N/inventory-management-PP3
+        ''', justify='center', style='cyan2'
+    )
+
+
 def main_menu():
     '''
     Main menu function
@@ -562,12 +618,13 @@ def main_menu():
         console.print(
             '''
              ------ MAIN MENU ------
-             ''', justify='center', style='light_steel_blue1')
+            ''', justify='center', style='light_steel_blue1')
         console.print(
             '''
         PRESS C TO ADD STOCK
         PRESS V TO VIEW STOCK,
         PRESS S TO SEARCH AND EDIT
+        PRESS I FOR INSTRUCTIONS
         PRESS Q TO QUIT\n
         ''', justify='center', style='cyan2')
         user_input = input()
@@ -580,11 +637,13 @@ def main_menu():
             view_stock_menu()
         elif user_input.lower() == 's':
             validate_search_data()
+        elif user_input.lower() == 'i':
+            display_info()
         elif user_input.lower() == 'q':
             quit_menu()
         else:
             console.print('Invalid input. Please select one of the available ',
-                          'options (C,V,S,Q)', justify='center', style='red')
+                          'options (C,V,S,I,Q)', justify='center', style='red')
 
 
 welcome_screen()
